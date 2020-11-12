33840efd4625f927a7b75d12cce056d4c6c3db77
The-Accolade <olusolaakolade@gmail.com>
2020-11-12 23:49:19 +0100

    updated product controller

--- server/routes/products.js
+++ server/routes/products.js
@@ -6,7 +6,8 @@ const productController = require('../api/controllers/products_controller');
 
 router.get('/', productController.getAll);
 router.post('/', productController.create);
-router.get('/:movieId', productController.getById);
-router.put('/:movieId', productController.updateById);
-router.delete('/:movieId', productController.deleteById);
+router.get('/:productId', productController.getById);
+router.put('/:productId', productController.updateById);
+router.delete('/:productId', productController.deleteById);
+
 module.exports = router;
